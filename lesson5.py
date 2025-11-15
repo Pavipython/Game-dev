f55ad8f1abaa2560125fa4d90448cd9bb59e89f6
Pavi <9002ivap@gmail.com>
2025-11-15 18:57:55 +0000

    alien game

--- lesson5.py
+++ lesson5.py
@@ -1,1 +1,28 @@
-print("hello")
+import pgzrun
+import random
+
+WIDTH=500
+HEIGHT=500
+
+reg=Actor("alien.png")
+reg.pos = (random.randint(0,500),random.randint(0,500))
+message = ""
+
+def draw():
+    screen.fill("light cyan")
+    screen.draw.text(message, color="black", center=(400,10))
+    reg.draw()
+
+def on_mouse_down(pos):
+    global message
+
+    if reg.collidepoint(pos):
+        reg.pos = (random.randint(0,500),random.randint(0,500))
+        message=("collided")
+    else:
+        message=("missed")
+    
+
+
+pgzrun.go()
+
