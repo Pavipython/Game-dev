ec25b6224485528f70ef5ba2ae217f03f98414b3
Pavi <9002ivap@gmail.com>
2025-11-29 18:53:37 +0000

    shapes

---
+++ shapes.py
@@ -0,0 +1,19 @@
+import pgzrun
+
+WIDTH=500
+HEIGHT=500
+
+def draw():
+    screen.fill("grey")
+    screen.draw.filled_circle(color=(255,0,0), radius=50, pos=(240,374))
+    screen.draw.line((140,0), (140,500), color=(0,100,100))
+
+    
+
+
+
+
+
+
+
+pgzrun.go()
