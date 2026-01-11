5d382d6e54a14dbe01b172eb4120208fff1a2470
Pavipython <9002ivap@gmail.com>
2026-01-11 13:59:49 +0000

    Add files via upload

--- itemcatch.py
+++ itemcatch.py
@@ -1,21 +1,23 @@
 import pgzrun
-import random
-WIDTH=800
-HEIGHT=500
+import random, pyautogui
+print(pyautogui.size())
+WIDTH, HEIGHT=pyautogui.size()
 
 items=["bagimg","batteryimg","bottleimg","cheeze","chipsimg"]
 level=1
 selected_items=[]
+print("global")
 
 def setup():
+    print("setup")
     for i in range(level+1):
         random_item=random.choice(items)
         actor_item=Actor(random_item)
         selected_items.append(actor_item)
         actor_item.pos=random.randint(50,700), 50
     
-    for item in selected_items:
-        animate(item, y = HEIGHT , duration=10)
+    # for item in selected_items:
+    #     animate(item, y = HEIGHT , duration=10)
 
 
 
@@ -23,11 +25,13 @@ def setup():
 setup()
 
 def draw():
+    #print("draw")
     screen.blit("new_bg.jpg", (0,0))
     for item in selected_items:
         item.draw()
 
 def update():
+    #print("appeal")
     pass
 
 pgzrun.go()
