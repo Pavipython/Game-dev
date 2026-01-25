510c1ea2491b59a7306f6cb9f2f1b6575358c7eb
Pavipython <9002ivap@gmail.com>
2026-01-25 13:55:17 +0000

    Add files via upload

--- itemcatch.py
+++ itemcatch.py
@@ -2,7 +2,7 @@ import pgzrun
 import random, pyautogui
 print(pyautogui.size())
 WIDTH, HEIGHT=pyautogui.size()
-
+HEIGHT-=100
 items=["bagimg","batteryimg","bottleimg","cheeze","chipsimg"]
 level=1
 selected_items=[]
@@ -24,17 +24,24 @@ print("global")
 
 # setup()
 animations=[]
+gamecomplete=False
+gameover=False
 
 def draw():
     #print("draw")
     screen.blit("new_bg.jpg", (0,0))
     for item in selected_items:
         item.draw()
+    if gameover:
+        screen.draw.text("gameover",(400,400))
+    elif gamecomplete:
+        screen.draw.text("congratulation",(400,400))
 
 def update():
     global selected_items
     #print("appeal")
-    selected_items=make_list(level)
+    if len(selected_items) == 0:
+        selected_items=make_list(level)
 
 def make_list(level):
     objects=options(level)
@@ -56,7 +63,7 @@ def sprite(objects):
     return sprites
 
 def givingpos(sprites):
-    gap=level+1# len(sprites)+1
+    gap=len(sprites)+1
     gap_size=WIDTH/gap
     random.shuffle(sprites)
     for i,v in enumerate(sprites):
@@ -66,11 +73,34 @@ def animation(sprites):
     global animations
     for i in sprites:
         duration=10-level
-        animations.append(animate(i,duration=duration,on_finished=gameover, y=HEIGHT))
+        i.anchor=("center", "bottom")
+        animations.append(animate(i,duration=duration,on_finished=gamend, y=HEIGHT))
 
-def gameover():
-    pass
-#   print("gameover")
+def gamend():
+    global gameover
+    gameover=True
+    stopanimation(animations)
+    print("gameover")
+def stopanimation(animations):
+    for i in animations:
+        if i.running:
+            i.stop()
+
+def on_mouse_down(pos):
+    global animations, selected_items, gamecomplete, level
+    for i in selected_items:
+        if i.collidepoint(pos):
+            if "paper" in i.image:
+                if level == 6:
+                    gamecomplete=True
+                    stopanimation(animations)
+                else:
+                    level+=1
+                    selected_items=[]
+                    animations=[]
+            else:
+                gamend()
+
 
 
 pgzrun.go()
